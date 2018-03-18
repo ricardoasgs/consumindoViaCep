3a7a90f6ebaeb15ab2e5ca89124bd16a55dd1234
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-03-18 17:26:05 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var request = require('request');
+var resposta = {};
+var port = 3000;
+
+app.get('/:cep', (req,res) => {
+    var cep = req.params.cep;
+    var endpoint = "https://viacep.com.br/ws/"+ cep +"/json/";
+    // console.log(endpoint)
+    request(endpoint, (error, response, body) => {
+        if(error || !body)
+            console.log('Ocorreu algum erro, tente novamente mais tarde.')
+        resposta = body;
+        res.send(resposta);
+    })
+    });
+
+app.listen(port, () => {console.log('ouvindo a porta: ',port)});
